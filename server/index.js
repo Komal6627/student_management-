ff60d4c470e62c284355459c8097e275dee50ac8
Komal6627 <komalkilledar1996@gmail.com>
2024-10-15 10:51:35 +0530

    mongodb connected successfully

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ app.use(cors());
 app.use(express.json());
 
 
+const url = process.env.MONGO_URL;
+
+mongoose.connect(url)
+  .then(() => console.log('MongoDB connected successfully'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
+
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server started on PORT ${process.env.PORT}`);
     
