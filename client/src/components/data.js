88800f13eaa54240a7d0f90e5df1ff9191638efe
Komal6627 <komalkilledar1996@gmail.com>
2024-10-17 10:03:00 +0530

    add the sample data

---
+++ client/src/components/data.js
@@ -0,0 +1,41 @@
+      //
+
+      {
+        "name" "John Doe",
+        "gender": "Male",
+        "dateOfBirth": "1994-05-15",
+        "address": "123 Main St,Mumbai",
+        "contactNo": 1234567890,
+        "email": "johndoe@school.com",
+        "password": "teacher",
+        "salary": 50000
+      },
+      
+      {
+        "name": "Marry Toney",
+        "gender": "Female",
+        "dateOfBirth": "1995-09-15",
+        "address": "Rose Appartment,lane no 3,Mumbai",
+        "contactNo": 1234567890,
+        "email": "marrytoney@school.com",
+        "password": "teacher",
+        "salary": 40000
+      }
+      
+      
+      //Student
+      
+        "name": "Jayant Killedar",
+        "contactNo": 9384375439,
+        "email": "jayantkilledar@school.com",
+        "password": "student"
+      
+        //admin
+      
+      {
+        "name": "Komal Killedar",
+        "contactNo": 8637460692,
+        "email": "komalkilledar@school.com",
+        "password": "admin"
+      }
+      
